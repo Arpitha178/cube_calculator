b376584943dad9a0535f8fd30050b52240f95128
Sri <arpithatshankar17@gmail.com>
2023-07-19 16:26:11 +0530

    first commit

--- cube.js
+++ cube.js
@@ -1,5 +1,11 @@
 function calculateCube(){
     var Num1=document.getElementById('Num1').value;
     var Results=Math.pow(Num1 , 3);
+    // var Results=Num1 * Num1 * Num1; 
     document.getElementById('Result').value=Results;
+}
+
+function reset(){
+    document.getElementById('Num1').value = ""
+    document.getElementById('Result').value = ""
 }
